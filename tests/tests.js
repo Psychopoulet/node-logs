bc21d63b58d7277241fad250c9d83e1b1ec00c8d
Sébastien Vidal <Sbastien Vidal>
2016-07-27 14:11:52 +0200

    split des logs trop longs

--- tests/tests.js
+++ tests/tests.js
@@ -7,123 +7,134 @@
 
 			fs = require("node-promfs"),
 
-			SimpleLogs = require(path.join(__dirname, "..", "lib", "main.js"));
+			NodeLogs = require(path.join(__dirname, "..", "lib", "main.js"));
 
 // private
 
-	var date = new Date(),
+	var sDir = path.join(__dirname, "logs"),
+		Logs = new NodeLogs(sDir),
+
+		date = new Date(),
 		sYear = date.getFullYear() + "",
-		sMonth = (9 < sMonth) ? sMonth + "" : "0" + sMonth,
-		sDay = (9 < sDay) ? sDay + "" : "0" + sDay,
+		sMonth = (9 < date.getMonth() + 1) ? date.getMonth() + 1 + "" : "0" + (date.getMonth() + 1),
+		sDay = (9 < date.getDate()) ? date.getDate() + "" : "0" + date.getDate();
 
-		sDir = path.join(__dirname, "logs"),
-		sFile = path.join(sDir, sYear + "_" + sMonth + "_" + sDay + ".html"),
+describe("errors", () => {
 
-		Logs = new SimpleLogs(sDir);
+	after(() => { return fs.rmdirpProm(sDir); });
 
-describe("errors", function() {
-
-	before(function(done) {
-		fs.rmdirpProm(Logs.pathDirLogs).then(done).catch(done);
-	});
-
-	it("should check pathDirLogs type value", function() {
-		assert.throws(function() { Logs.pathDirLogs = 15; }, Error, "check type value does not throw an error");
-		assert.doesNotThrow(function() { Logs.pathDirLogs = sDir; }, Error, "check type value throw an error");
+	it("should check pathDirLogs type value", () => {
+		assert.throws(() => { Logs.pathDirLogs = 15; }, Error, "check type value does not throw an error");
+		assert.doesNotThrow(() => { Logs.pathDirLogs = sDir; }, Error, "check type value throw an error");
 	});
 
 });
 
-describe("write", function() {
+describe("write", () => {
 
-	describe("log", function() {
+	before(() => { return fs.rmdirpProm(sDir); });
 
-		it("should test function", function() {
+	it("should test log function", () => {
 
-			return Logs.log("log").then(function() {
-				return Logs.log({ test: "test" });
-			}).then(function() {
-				return Logs.log([ "01", "02", "03" ]);
-			});
-
+		return Logs.log("log").then(() => {
+			return Logs.log({ test: "test" });
+		}).then(() => {
+			return Logs.log([ "01", "02", "03" ]);
 		});
 
 	});
 
-	describe("info", function() {
+	it("should test info function", () => {
 
-		it("should test function", function() {
+		return Logs.info("info");
 
-			return Logs.info("info");
+	});
 
+	it("should test success function", () => {
+
+		return Logs.ok("ok").then(() => {
+			return Logs.success("success");
 		});
 
 	});
 
-	describe("success", function() {
+	it("should test warning function", () => {
 
-		it("should test function", function() {
-
-			return Logs.ok("ok").then(function() {
-				return Logs.success("success");
-			});
-
+		return Logs.warn("warn").then(() => {
+			return Logs.warning("warning");
 		});
 
 	});
 
-	describe("warning", function() {
+	it("should test error function", () => {
 
-		it("should test function", function() {
-
-			return Logs.warn("warn").then(function() {
-				return Logs.warning("warning");
-			});
-
-		});
-
-	});
-
-	describe("error", function() {
-
-		it("should test function", function() {
-
-			return Logs.err("err").then(function() {
-				return Logs.error("error");
-			});
-
+		return Logs.err("err").then(() => {
+			return Logs.error("error");
 		});
 
 	});
 
 });
 
-describe("read", function() {
+describe("read", () =>  {
 
-	it("should return registered log files", function() {
+	it("should return the last writable file", () =>  {
 
-		return Logs.getLogs().then(function(logs) {
-			assert.strictEqual(true, logs instanceof Object, "returned value is not an Array");
-			return Logs.read(sYear, sMonth, sDay);
-		}).then(function(txt) {
+		return Logs.lastWritableFile().then((lastwritablefile) =>  {
+			assert.strictEqual("string", typeof lastwritablefile, "returned value is not a string");
+		});
+
+	});
+
+	it("should return registered log files", () =>  {
+
+		return Logs.getLogs().then((logs) =>  {
+
+			assert.strictEqual(true, logs instanceof Object, "returned value is not an Object");
+			assert.strictEqual(true, logs[sYear] && logs[sYear] instanceof Object, "returned value is not an Object with year");
+			assert.strictEqual(true, logs[sYear][sMonth] && logs[sYear][sMonth] instanceof Object, "returned value is not an Object with month");
+			assert.strictEqual(true, logs[sYear][sMonth][sDay] && logs[sYear][sMonth][sDay] instanceof Object, "returned value is not an Object with day");
+
+			return Logs.read(sYear, sMonth, sDay, 1);
+
+		}).then((txt) =>  {
+
 			assert.strictEqual("string", typeof txt, "returned value is not a string");
+			assert.strictEqual("<table class=\"node-logs\">", txt.substring(0, "<table class=\"node-logs\">".length), "returned value is not correctely formated");
+			assert.strictEqual("</table>", txt.substring(txt.length - "</table>".length, txt.length), "returned value is not correctely formated");
+
 		});
 
 	});
 
 });
 
-describe("delete", function() {
+describe("delete", () =>  {
 
-	after(function(done) {
-		fs.rmdirpProm(sDir).then(done).catch(done);
-	});
+	after(() => { return fs.rmdirpProm(sDir); });
 
-	it("should delete log file", function() {
+	it("should delete log file", () =>  {
 
-		return Logs.remove(sYear, sMonth, sDay).then(function() {
+		return Logs.getLogs().then((logs) =>  {
 
-			assert.strictEqual(false, fs.isFileSync(sFile), "log file was not deleted");
+			function _delete(filenumber) {
+
+				(1, console).log(filenumber, logs[sYear][sMonth][sDay][filenumber]);
+
+				return Logs.remove(sYear, sMonth, sDay, logs[sYear][sMonth][sDay][filenumber]).then(() =>  {
+
+					if (filenumber < logs.length) {
+						return _delete(filenumber + 1);
+					}
+					else {
+						return Promise.resolve();
+					}
+					
+				});
+
+			}
+
+			return _delete(0);
 
 		});
 
