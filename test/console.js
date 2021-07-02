d8390ecdd414aa05ff419d99117497121fb676d1
Sébastien Vidal <svidal1@free.fr>
2021-07-02 12:04:41 +0200

    console without promise

--- test/console.js
+++ test/console.js
@@ -3,73 +3,55 @@
 // deps
 
 	// locals
-	const _console = require(require("path").join(__dirname, "..", "lib", "console.js"));
+	const consoleLogger = require(require("path").join(__dirname, "..", "lib", "consoleLogger.js"));
 
 // tests
 
-describe("_console", () => {
+describe("consoleLogger", () => {
 
 	it("should check empty message", () => {
-		return _console("");
+		return consoleLogger("");
 	});
 
 	it("should check different types", () => {
 
-		return _console("test", []).then(() => {
-			return _console("log", [ "log" ]);
-		}).then(() => {
-			return _console("information", [ "information" ]);
-		}).then(() => {
-			return _console("success", [ "success" ]);
-		}).then(() => {
-			return _console("warning", [ "warning" ]);
-		}).then(() => {
-			return _console("error", [ "error" ]);
-		});
+		consoleLogger("test", []);
+		consoleLogger("log", [ "log" ]);
+		consoleLogger("information", [ "information" ]);
+		consoleLogger("success", [ "success" ]);
+		consoleLogger("warning", [ "warning" ]);
+		consoleLogger("error", [ "error" ]);
 
 	});
 
 	it("should check dupplicates options types", () => {
 
-		return _console("test", []).then(() => {
-			return _console("log", [ "log", "log" ]);
-		}).then(() => {
-			return _console("information", [ "information", "information" ]);
-		}).then(() => {
-			return _console("success", [ "success", "success" ]);
-		}).then(() => {
-			return _console("warning", [ "warning", "warning" ]);
-		}).then(() => {
-			return _console("error", [ "error", "error" ]);
-		});
+		consoleLogger("test", []);
+		consoleLogger("log", [ "log", "log" ]);
+		consoleLogger("information", [ "information", "information" ]);
+		consoleLogger("success", [ "success", "success" ]);
+		consoleLogger("warning", [ "warning", "warning" ]);
+		consoleLogger("error", [ "error", "error" ]);
 
 	});
 
 	it("should check different options", () => {
 
-		return _console("background", [ "log", "background" ]).then(() => {
-			return _console("bold", [ "log", "bold" ]);
-		}).then(() => {
-			return _console("italic", [ "log", "italic" ]);
-		}).then(() => {
-			return _console("strikethrough", [ "log", "strikethrough" ]);
-		}).then(() => {
-			return _console("underline", [ "log", "underline" ]);
-		});
+		consoleLogger("background", [ "log", "background" ]);
+		consoleLogger("bold", [ "log", "bold" ]);
+		consoleLogger("italic", [ "log", "italic" ]);
+		consoleLogger("strikethrough", [ "log", "strikethrough" ]);
+		consoleLogger("underline", [ "log", "underline" ]);
 
 	});
 
 	it("should check different types with background", () => {
 
-		return _console("log", [ "log", "background" ]).then(() => {
-			return _console("information", [ "information", "background" ]);
-		}).then(() => {
-			return _console("success", [ "success", "background" ]);
-		}).then(() => {
-			return _console("warning", [ "warning", "background" ]);
-		}).then(() => {
-			return _console("error", [ "error", "background" ]);
-		});
+		consoleLogger("log", [ "log", "background" ]);
+		consoleLogger("information", [ "information", "background" ]);
+		consoleLogger("success", [ "success", "background" ]);
+		consoleLogger("warning", [ "warning", "background" ]);
+		consoleLogger("error", [ "error", "background" ]);
 
 	});
 
